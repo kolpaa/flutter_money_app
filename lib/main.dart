8a8b46d9de02d9c3741553e95c89c7dfafc36ba2
Ilya Kolpakov <kolpakoph@gmail.com>
2022-02-26 13:15:35 +0300

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -57,7 +57,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Flutter App'),
+        title: Text('Money App'),
         actions: [
           IconButton(
             onPressed: () {
